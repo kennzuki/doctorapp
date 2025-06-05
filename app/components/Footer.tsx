eb5159d9eadc4baf9ee1275f738ebb3221911dd2
kennzuki <kingsyken@gmail.com>
2025-06-06 02:20:28 +0300

    footer ui

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className='footer footer-center p-10 bg-neutral text-neutral-content'>
+      <div className='flex justify-between items-center'>
+        <h1>Kenki Appointments</h1>
+        <p>Copyright © 2023 - All right reserved by Kenki</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
